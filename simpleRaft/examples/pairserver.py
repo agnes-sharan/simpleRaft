724534c58cf3c5de5612bbac37df2d61e306f4bc
Tarun-Karthik <tarunkarthik999@gmail.com>
2018-10-05 12:32:31 +0530

    example code for client and server

---
+++ simpleRaft/examples/pairserver.py
@@ -0,0 +1,20 @@
+import zmq
+import random
+import sys
+import time
+
+
+port = "5556"
+context = zmq.Context()
+socket = context.socket(zmq.PAIR)
+socket.bind("tcp://*:%s" % port)
+
+
+while True:
+	socket.send("Sever message to client3")
+	msg = socket.recv()
+	print msg
+	time.sleep(1)
+
+
+
